5d8c835d261735f74bccf0db777645ad7eedc17d
rohitjose <rohitjose@gmail.com>
2015-07-12 18:13:29 -0700

    More changes July 12

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import SignUpForm
 def home(request):
-	return render(request,"home.html",{})
+	form = SignUpForm(request.POST or None)
+
+	if form.is_valid():
+		instance = form.save(commit=False)
+		print instance.email
+		print instance.full_name
+
+	context = {
+		"title": request.user,
+		"form" : form,
+	}
+	
+	if request.method == "POST":
+		print request.POST
+	return render(request,"home.html",context)
